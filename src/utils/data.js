57c91ee3824386bad3a0ace97a4000573d0593e2
Dani Lucaci <danilucaci@gmail.com>
2021-04-22 16:55:52 +0200

    feat: initial commit with react intro

---
+++ src/utils/data.js
@@ -0,0 +1,40 @@
+export function getPosts() {
+  return [
+    {
+      id: 2,
+      title: "post 2",
+      description:
+        "Qui duis cupidatat id in irure tempor quis duis anim eiusmod quis. Ex culpa anim in ea proident velit magna et sint sit magna. Fugiat exercitation et sit duis ad do mollit sit non dolor ex laborum qui et. Reprehenderit est occaecat eu exercitation labore esse aliqua excepteur amet id fugiat reprehenderit dolor. Consequat est velit incididunt commodo duis sint laborum occaecat aliquip cillum nostrud. Est ea dolore incididunt occaecat enim qui dolor ipsum.",
+    },
+    {
+      id: 1,
+      title: "post 1",
+      description:
+        "Aliqua excepteur id fugiat qui sunt. Mollit aliquip cupidatat pariatur velit ex eiusmod quis dolore enim quis esse non. Fugiat laboris eu esse laborum cillum non sunt velit magna occaecat nisi. Aliqua consectetur cillum cupidatat Lorem sunt id eiusmod nulla. Excepteur laboris consectetur adipisicing aute. Mollit sunt occaecat laboris ex mollit sit excepteur officia laborum duis amet anim non.",
+    },
+    {
+      id: 3,
+      title: "post 3",
+      description:
+        "Ex veniam laborum adipisicing minim veniam sint Lorem ipsum irure cillum duis. Fugiat cupidatat aute pariatur elit culpa aute anim aute exercitation eu eu nulla amet excepteur. Occaecat ea eiusmod cupidatat id commodo dolore nisi anim irure reprehenderit. Incididunt id eu non aliquip tempor officia. Excepteur dolore id incididunt ut sit id eu duis.",
+    },
+    {
+      id: 4,
+      title: "post 4",
+      description:
+        "Quis Lorem veniam aute id. Id voluptate occaecat sint sit tempor veniam irure consequat ipsum cillum labore. Culpa magna commodo excepteur sint proident elit aliqua officia ad.",
+    },
+    {
+      id: 5,
+      title: "post 5",
+      description:
+        "Consequat do ut voluptate nulla nostrud commodo consequat exercitation et amet enim. Ea quis minim pariatur reprehenderit sint minim enim eu deserunt qui. Eiusmod incididunt nulla est proident sit est excepteur culpa adipisicing culpa officia. Officia veniam ea enim ea ut ex occaecat ipsum. Voluptate quis duis magna proident dolor pariatur voluptate tempor ipsum elit ex sit. Sit dolore mollit culpa aliquip excepteur anim.",
+    },
+    {
+      id: 6,
+      title: "post 6",
+      description:
+        "Eiusmod anim occaecat non qui Lorem veniam voluptate elit. Minim do duis ad irure consequat. In quis aliquip dolore laborum occaecat eu nostrud labore commodo.",
+    },
+  ];
+}
