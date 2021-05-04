52f99ea9d4e3f11d4810f60c563754b2acb125d6
Dani Lucaci <danilucaci@gmail.com>
2021-05-04 10:52:18 +0200

    docs: add final forms workshop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Main from "./components/Main";
+import Input from "./components/Input";
+import Button from "./components/Button";
 
-function App() {
-  const count = 1;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+    };
 
-  return (
-    <Main>
-      <section className="row">
-        <div className="col col-12">
-          <h1>The current count is: {count}</h1>
-        </div>
-      </section>
-    </Main>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      firstName: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(this.state);
+  }
+
+  render() {
+    const { firstName } = this.state;
+    return (
+      <Main>
+        <section className="row">
+          <div className="col col-12">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <Input
+                  label="First name"
+                  placeholder="Your first name"
+                  type="text"
+                  id={firstName}
+                  name={firstName}
+                  value={firstName}
+                  handleChange={this.handleChange}
+                />
+              </div>
+              <Button submitButton>Submit</Button>
+            </form>
+          </div>
+          <div className="col col-12 mt-4">
+            <pre className="bg-light p-3">
+              <code>{JSON.stringify(this.state, null, 2)}</code>
+            </pre>
+          </div>
+        </section>
+      </Main>
+    );
+  }
 }
 
 export default App;
